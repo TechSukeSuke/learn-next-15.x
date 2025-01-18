db742afe355bf887766baee4d3f96bb8725f755a
TechSukeSuke <business.k.shimizu@gmail.com>
2025-01-18 11:48:43 +0900

    feat: dev indicators.

--- apps/next-15/next.config.js
+++ apps/next-15/next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    // About react compiler
     reactCompiler: {
       compilationMode: 'annotation',
     },
+  },
+
+  // About devIndicators
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: 'bottom-right',
   },
 };
 
